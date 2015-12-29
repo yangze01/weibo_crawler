a8a206d39f8b282379d18b921e9bce8910eae522
yangze01 <858848101@qq.com>
2015-12-29 19:47:23 +0800

    the struct for sina_weibo

---
+++ weibo_crawler/struc.py
@@ -0,0 +1,19 @@
+class all_url:
+    def __init__(self):
+        self.userid = 3199208303
+        self.home_page = "http://weibo.cn/"
+        self.weibourl = self.home_page+str(self.userid)+"/profile?page="
+        self.followurl = self.home_page+str(self.userid)+"/follow?page="
+        self.fansurl = self.home_page+str(self.userid)+"/fans?page="
+
+class per_deque:
+    def __init__(self):
+        self.id_queue=deque()
+        self.name_queue=deque()
+
+class per_struct:
+    def __init__(self):
+        self.fans_set = set()
+        self.follow_set = set()
+        self.friends_set = set()
+        self.all_set = set()
