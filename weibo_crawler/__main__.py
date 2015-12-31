59c1a43b3c3c182bec1da8d925c879540e36233e
yangze01 <858848101@qq.com>
2015-12-31 20:00:21 +0800

    111

--- weibo_crawler/__main__.py
+++ weibo_crawler/__main__.py
@@ -2,25 +2,24 @@
 from get_data import *
 from login import *
 from struc import *
+from collections import deque
 
 if __name__=="__main__":
     driver = getLoginDriver(18330274826,523581600)
     time.sleep(3)
     headers = getHeaders(driver)
-
+#   init two struct:queue and set
+    queue = deque()
+    visited = set()
+    queue.append(3199208303)
     use_url = all_url()
     this_url = use_url.fansurl+str(2)
 
     req = urllib2.Request(this_url,headers=headers)
     try:
-        response = urllib2.urlopen(req)
-        data = response.read()
-
-        # file = open("test1.html","w")
-        # file.write(data)
-        # file.close()
-
-        print data
+        while deque:
+            catch_id = queue.popleft()#pop the left var
+            
 
     except urllib2.URLError,e:
         if hasattr(e,"code"):
