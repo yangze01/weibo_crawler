0530485aad685694303ad924ef0c1fd2f015e45d
yangze01 <858848101@qq.com>
2015-12-31 19:57:16 +0800

    test

--- weibo_crawler/login.py
+++ weibo_crawler/login.py
@@ -8,7 +8,8 @@ import time
 reload(sys)
 sys.setdefaultencoding('utf-8')
 # Create a new instance of the Chrome driver
-  
+# the test push
+
 def getLoginDriver(username,password):
     driver = webdriver.Chrome()
 
