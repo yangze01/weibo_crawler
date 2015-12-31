bc851c9ee72ff64a66aa220a118cff826866310b
warriorZH <zhangpeihua123@gmail.com>
2015-12-31 17:07:01 +0800

    add con2mong foler and set interface for operations on blog data to pymongo

---
+++ weibo_crawler/get_blogcont/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*- coding: UTF-8 -*-
+
+#filename: __init__.py
+#description: initial file for get_blogcont package
+#author: warrior  ,mail: oowarrioroo@163.com
+
+"""
+    package for get weibo blog -- content ,comment ,Forwarding ,praise
+"""
