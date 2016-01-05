5ffa54619d75f93a4a7b4677abc26787235ed6b9
warriorZH <zhangpeihua123@gmail.com>
2016-01-05 18:59:13 +0800

    add more detail data structure for blog_unit after overview practical blog data

--- weibo_crawler/con2mongo/blogUnit.py
+++ weibo_crawler/con2mongo/blogUnit.py
@@ -8,8 +8,8 @@
 #log:
 #--- 2016-1-1 : add {"blog_device": ""} into blog_unit, record a blog be sended from what device
 #               add {"repost_device": ""}, {"repost_time": ""}, {"repost_attitude": ""},
-#               add {"comment_device": ""}, {"comment_time": ""}, {"comment_attitude": ""}, 
-
+#               add {"comment_device": ""}, {"comment_time": ""}, {"comment_attitude": ""},
+#--- 2016-1-5 : add more detail data structure for blog_unit after overview practical blog data
 """
     set unit form of blog
 """
@@ -30,25 +30,58 @@ class blogUnit(object):
         self.blog_unit = {
             "user_ID": "",
             "blog": {
-            "blog_id": "",
-            "blog_address": "",
-            "blog_time": "",
-            "blog_device": "",
-            "blog_content": ""
+                "blog_id": "",
+                "blog_address": "",
+                "time": "",
+                "device": "",
+                "content":
+                {
+                    "topic_name": [],
+                    "topic_content": [],
+                    "mention_name": [],
+                    "mention_content": [],
+                    "url_name": [],
+                    "url_content": [],
+                    "text_content": ""
+                },
+                "blog_attitude": ""
             },
-            "repost": {
-            "repost_id": "",
-            "repost_content": "",
-            "repost_device": "",
-            "repost_time": "",
-            "repost_attitude": ""
-            },
-            "comment": {
-            "comment_id": "",
-            "comment_content": "",
-            "comment_device": "",
-            "comment_time": "",
-            "comment_attitude": ""
-            },
-            "blog_attitude": ""
+            "repost": [
+                {
+                    "repost_id": "",
+                    "repost_user_id": "",
+                    "content":
+                    {
+                        "topic_name": [],
+                        "topic_content": [],
+                        "mention_name": [],
+                        "mention_content": [],
+                        "url_name": [],
+                        "url_content": [],
+                        "text_content": ""
+                    },
+                    "device": "",
+                    "time": "",
+                    "repost_attitude": ""
+                },
+            ]
+            "comment": [
+                {
+                    "comment_id": "",
+                    "comment_user_id": "",
+                    "content":
+                    {
+                        "topic_name": [],
+                        "topic_content": [],
+                        "mention_name": [],
+                        "mention_content": [],
+                        "url_name": [],
+                        "url_content": [],
+                        "text_content": ""
+                    },
+                    "device": "",
+                    "time": "",
+                    "comment_attitude": ""
+                }
+            ]
         }
