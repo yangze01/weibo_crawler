13641a43c6cdcf43724d6af90254b011942c8037
= <=>
2016-01-11 21:08:20 +0800

    push the test page

--- weibo_crawler/test/getinfo.py
+++ weibo_crawler/test/getinfo.py
@@ -2,14 +2,55 @@
 import re
 import urllib
 import urllib2
-def get_userinfo(data):
-    re_allinfo='<div class="c">.*?<br/></div>'
-    #re_allinfo="(?<=会员等级：).*?(?=&nbsp)"
-    pattern = re.compile(re_allinfo,re.S)
-    items = re.findall(pattern,data)
-    return items
+import sys
+from struc import *
+reload(sys)
+sys.setdefaultencoding('utf-8')
 def get_data(this_url,headers):
     req = urllib2.Request(this_url,headers=headers)
     response = urllib2.urlopen(req)
     data = response.read()
     return data
+
+def get_userinfo(userid,headers):
+    this_url = "http://weibo.cn/"+userid+"/info"
+    userdata = userinfo()
+    data = get_data(this_url,headers)
+    #print data
+
+    re_allinfo='<div class="c">.*?<br/></div>'
+    pattern = re.compile(re_allinfo,re.S)
+    item = re.findall(pattern,data)[0]
+
+    re_vip = "(?<=会员等级：).*?(?=&nbsp)"
+    re_username="(?<=昵称:).*?(?=<br/>)"
+    re_certificate="(?<=认证:).*?(?=<br/>)"
+    re_sex="(?<=性别:).*?(?=<br/>)"
+    re_district="(?<=地区:).*?(?=<br/>)"
+    re_birthday="(?<=生日:).*?(?=<br/>)"
+    re_certimes="(?<=认证信息：).*?(?=<br/>)"
+
+    pat_vip = re.compile(re_vip,re.S)
+    pat_username = re.compile(re_username,re.S)
+    pat_certificate = re.compile(re_certificate,re.S)
+    pat_sex = re.compile(re_sex,re.S)
+    pat_district = re.compile(re_district,re.S)
+    pat_birthday = re.compile(re_birthday,re.S)
+    pat_certimes = re.compile(re_certimes,re.S)
+    #if re.match(pat_vip,item):
+    #    print re.findall(pat_vip,item)[0]
+    if re.findall(pat_vip,item):
+        userdata.info["vip"] = re.findall(pat_vip,item)[0]
+    if re.findall(pat_username,item):
+        userdata.info["username"] = re.findall(pat_username,item)[0]
+    if re.findall(pat_certificate,item):
+        userdata.info["certificate"] = re.findall(pat_certificate,item)[0]
+    if re.findall(pat_sex,item):
+        userdata.info["sex"] = re.findall(pat_sex,item)[0]
+    if re.findall(pat_district,item):
+        userdata.info["district"] = re.findall(pat_district,item)[0]
+    if re.findall(pat_birthday,item):
+        userdata.info["birthday"] = re.findall(pat_birthday,item)[0]
+    if re.findall(pat_certimes,item):
+        userdata.info["certimes"] = re.findall(pat_certimes,item)[0]
+    return userdata.info
---
+++ weibo_crawler/test/struc.py
@@ -0,0 +1,43 @@
+#-*- coding: UTF-8 -*-
+from collections import deque
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class all_url:
+    def __init__(self):
+        self.userid = 3199208303
+        self.home_page = "http://weibo.cn/"
+        self.weibourl = self.home_page+str(self.userid)+"/profile?page="
+        self.followurl = self.home_page+str(self.userid)+"/follow?page="
+        self.fansurl = self.home_page+str(self.userid)+"/fans?page="
+
+class per_deque:
+    def __init__(self):
+        self.id_queue=deque()
+        self.name_queue=deque()
+
+class per_struct:
+    def __init__(self):
+        self.fans_set = set()
+        self.follow_set = set()
+        self.friends_set = set()
+        self.all_set = set()
+
+class userinfo(object):
+    def __init__(self):
+        self.relation={
+            "fans": "",
+            "follow": "",
+            "union": "",
+            "intersection": ""
+        }
+        self.info={
+            "vip":"",
+            "username":"",
+            "certificate":"",
+            "sex":"",
+            "district":"",
+            "birthday":"",
+            "certimes":""
+        }
