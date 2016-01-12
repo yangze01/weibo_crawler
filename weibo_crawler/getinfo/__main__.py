db25aae3e197223ccc75c94a81d4dc545efb1f42
= <=>
2016-01-12 15:59:50 +0800

    使用List解决逐个存储的弊端

--- weibo_crawler/getinfo/__main__.py
+++ weibo_crawler/getinfo/__main__.py
@@ -5,28 +5,27 @@ from struc import *
 from collections import deque
 
 if __name__=="__main__":
-    driver = getLoginDriver(183302748)
+    driver = getLoginDriver(18330274826,523581600)
     time.sleep(3)
     headers = getHeaders(driver)
     #123456
 #   init two struct:queue and set
-    queue = deque()
-    testqueue = deque()
+    queue = list()
+
     visited = set()
     queue.append("2890733820")
     #print queue
     try:
         while queue:
-            catch_id = queue.popleft()#取出待爬取的id
-            visited |= {catch_id}  # 标记为已访问
+            catch_id = queue.pop()#取出待爬取的id
             print catch_id
+            visited |= {catch_id}  # 标记为已访问
+            print visited
             tmp_userinfo = userinfo()
             tmp_userinfo.info = get_userinfo(catch_id,headers)
-            tmp_userinfo.relation = get_relation(catch_id,headers)
             print tmp_userinfo.info
-            print tmp_userinfo.relation["fans"]
-            for item in tmp_userinfo.relation["fans"]:
-                print item
+            tmp_userinfo.relation = get_relation(catch_id,headers)
+            queue = tmp_userinfo.relation["intersection"]+queue
     except urllib2.URLError,e:
         if hasattr(e,"code"):
             print e.code
