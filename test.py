c8b63f774767dd13396e0211a88e1ef931cd7174
= <=>
2016-01-12 15:19:17 +0800

    将获取用户信息的模块放在一个目录下

---
+++ test.py
@@ -0,0 +1,3 @@
+a=["123123","adasd","adasqw","qwew"]
+while a:
+    print a.pop()
--- weibo_crawler/getinfo/__main__.py
+++ weibo_crawler/getinfo/__main__.py
@@ -16,13 +16,17 @@ if __name__=="__main__":
     queue.append("2890733820")
     #print queue
     try:
-        
-        catch_id = queue.popleft()
-        print catch_id
-        tmp_userinfo = userinfo()
-        tmp_userinfo.info = get_userinfo(catch_id,headers)
-        tmp_userinfo.relation = get_relation(catch_id,headers)
-
+        while queue:
+            catch_id = queue.popleft()#取出待爬取的id
+            visited |= {catch_id}  # 标记为已访问
+            print catch_id
+            tmp_userinfo = userinfo()
+            tmp_userinfo.info = get_userinfo(catch_id,headers)
+            tmp_userinfo.relation = get_relation(catch_id,headers)
+            print tmp_userinfo.info
+            print tmp_userinfo.relation["fans"]
+            for item in tmp_userinfo.relation["fans"]:
+                print item
     except urllib2.URLError,e:
         if hasattr(e,"code"):
             print e.code
